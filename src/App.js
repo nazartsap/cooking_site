76fb627cbc0da657d7336967bbff656afed2d7cd
nazartsap <tsap.n.i20@gmail.com>
2024-01-04 22:46:29 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-// App.js
 import React, { useState } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
-import IngredientList from './components/IngredientList';
-import RecipeList from './components/RecipeList';
-import PopularRecipes from './components/PopularRecipes';
+import Home from './components/Home'
 
 const App = () => {
-  const [selectedIngredients, setSelectedIngredients] = useState([]);
-  const [recipes, setRecipes] = useState([]);
-  const [popularRecipes, setPopularRecipes] = useState(['рецепт 1', 'рецепт 2', 'рецепт 3']);
-
-  const handleIngredientSelect = (ingredient) => {
-    setSelectedIngredients([...selectedIngredients, ingredient]);
-    generateRecipes([...selectedIngredients, ingredient]);
-  };
-
-  const generateRecipes = (selectedIngredients) => {
-    const generatedRecipes = [
-      `${selectedIngredients.join(', ')}`,
-      `Another recipe with ${selectedIngredients.join(', ')}`,
-    ];
-    setRecipes(generatedRecipes);
-  };
-
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div style={{ display: 'flex',justifyContent:'space-around', alignItems: 'center' }}>
-        <IngredientList 
-          ingredients={['Яйцо', 'Мука', 'Марковка']}
-          onSelect={handleIngredientSelect}
-        />
-        <RecipeList recipes={recipes} />
-        <PopularRecipes popularRecipes={popularRecipes} />
-      </div>
-    </DndProvider>
+    <Home/>
   );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-const Home = () =>{
+import React, { useState } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+import IngredientList from './IngredientList';
+import RecipeList from './RecipeList';
+import PopularRecipes from './PopularRecipes';
+
+const Home = () => {
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [popularRecipes, setPopularRecipes] = useState(['рецепт 1', 'рецепт 2', 'рецепт 3']);
+
+  const handleIngredientSelect = (ingredient) => {
+    setSelectedIngredients([...selectedIngredients, ingredient]);
+    generateRecipes([...selectedIngredients, ingredient]);
+  };
+
+  const generateRecipes = (selectedIngredients) => {
+    const generatedRecipes = [
+      `${selectedIngredients.join(', ')}`,
+      `Another recipe with ${selectedIngredients.join(', ')}`,
+    ];
+    setRecipes(generatedRecipes);
+  };
+
   return (
-    <div>
-      <h3>Dogs</h3>
-      <div>
-        <img src="./dog.png"/>
-        <img src="./dog.png"/>
+    <DndProvider backend={HTML5Backend}>
+      <div style={{ display: 'flex',justifyContent:'space-around', alignItems: 'center' }}>
+        <IngredientList 
+          ingredients={['Яйцо', 'Мука', 'Марковка']}
+          onSelect={handleIngredientSelect}
+        />
+        <RecipeList recipes={recipes} />
+        <PopularRecipes popularRecipes={popularRecipes} />
       </div>
-    </div>
+    </DndProvider>
   );
-}
+};
+
 export default Home;
