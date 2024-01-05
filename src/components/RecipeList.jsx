894daeddcf6d7db082b5b56eaaef70836ae65ef7
nazartsap <tsap.n.i20@gmail.com>
2024-01-05 16:06:16 +0300

    new style

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../styles/RecipeList.css'
 const RecipeList = ({ recipes }) => {
   return (
-    <div>
+    <div className='recipe_list_block'>
       <h2>Рецепты</h2>
+      <h2>
+        <input className = 'find_placeholder' type="text" placeholder="Найди рецепты"  />
+      </h2>
       <ul>
         {recipes.map((recipe, index) => (
           <li key={index}>{recipe}</li>
